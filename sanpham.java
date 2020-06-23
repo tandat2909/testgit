d1f913eea50d7c6e8f1b2595df4d1dfe00446e36
ITMan <datoanh25032017@gmai.com>
2020-06-23 14:53:03 +0700

    test

---
+++ sanpham.java
@@ -0,0 +1,16 @@
+public class sanpham{
+	private String masp;
+	private String tensp;
+
+	public sanpham(){
+		
+	}
+	public sanpham(String msp , String tensp){
+	this.masp = msp;
+	this.tensp = tensp;
+	s
+	}
+	public String toString(){
+		return "Ma sp: " + masp + "\nTen sp: " + tensp;
+	}
+}
---
+++ test.java
@@ -0,0 +1,11 @@
+import sanpham;
+public class test{
+
+public static void main(String[] aasd){
+	sanpham sp1 = new sanpham("1" , "cho");
+	sanpham sp2 = new sanpham("2", "meo");
+	System.out.println(sp1);
+	System.out.println(sp2);
+
+}
+}
